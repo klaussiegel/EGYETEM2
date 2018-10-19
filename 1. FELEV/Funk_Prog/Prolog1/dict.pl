414f167f030d209fe6815eb91a13229d70b23f97
Tomi Olah <tomiolah1998@gmail.com>
2018-10-19 14:04:03 +0300

    Prolog + Java L3 ✅

--- 1. FELEV/Funk_Prog/Prolog1/dict.pl
+++ 1. FELEV/Funk_Prog/Prolog1/dict.pl
@@ -60,6 +60,165 @@ invertal([A|B],L) :-
 %       torolk([1,2,3,4,5,6,7,8],2,[1,3,5,7])
 % predikátum.
 
-torolk([],_,[]).
-torolk([A|B],0,B).
-torolk([A|B],K,L) :-
+torolk(In,K,Out) :-
+    same_length(In,[_|Out]),
+    append(Pre,[_|Suf],In),
+    length([_|Pre],K),
+    append(Pre,Suf,Out).
+
+% 7. Teszteljük, hogy egy lista lehet-e permutációja az
+% [1..N] közötti számoknak, ahol N a lista hossza:
+%  minden eleme az 1 és N között van.
+% Például:
+%       perm([1,2,3,4]) -> true, illetve
+%       perm([1,2,4,4]) -> false.
+%
+% Javaslat:
+%       számítsuk ki a lista hosszát, generáljuk az 1..N közötti elemek listáját,
+%       majd teszteljük, hogy a két lista egymás permutációja-e.
+
+len([],0).
+len([_|Xs],L) :-
+    len(Xs,N),
+    L is N+1.
+
+is_permutation(Xs, Ys) :-
+    msort(Xs, Sorted),
+    msort(Ys, Sorted).
+
+perm(L) :-
+    len(L,LL),
+    numlist(1, LL, Ctrl),
+    is_permutation(L,Ctrl).
+
+% 8. Alakítsunk át egy listát kompakt formába:
+% az ismétlődő egymásutáni atomokat helyettesítsük egy olyan kételemű listával,
+% melyben az első az atom, a második az atom sokszorossága faktora.
+%
+% Amennyiben az atom számossága 1, akkor egyszerűen másoljuk át az elemet a "kimenet"-be.
+%
+% Például:
+%       A kompakt([a,a,a,b,a,a,c,c,b,d,d,d,d],[[a,3],b,[a,2],[c,2],b,[d,4]]).
+%       predikátum igaz kell, hogy legyen. (kezeljük az összes esetet)
+
+felsorol([],[]).
+felsorol([H|T],[[H,1]|Out]) :-
+    felsorol(T,Out).
+
+osszecsuk([],[]).
+osszecsuk([X],[X]).
+osszecsuk([[X,A1],[X,A2]|T],Out) :-
+    N is A1+A2,
+    osszecsuk([[X,N]|T],Out).
+
+osszecsuk([[X,A1],[Y,A2]|T],[[X,A1]|Out]) :-
+    X \= Y,
+    osszecsuk([[Y,A2]|T],Out).
+
+kompakt(L,Out) :-
+    felsorol(L,X),
+    osszecsuk(X,Out).
+
+
+% 9. Tekintsük az alábbi logikai feladatot:
+%     S E N D   +
+%     M O R E  =
+%   ----------
+%   M O N E Y
+%
+% Tudva, hogy a fenti betűk különböző számjegyeket kódolnak úgy,
+% hogy egyik első számjegy sem nulla, találjuk meg a megoldásokat.
+
+% D E M N O R S Y
+
+szam(0).
+szam(1).
+szam(2).
+szam(3).
+szam(4).
+szam(5).
+szam(6).
+szam(7).
+szam(8).
+szam(9).
+
+megold(L) :-
+    L = [D,E,M,N,O,R,S,Y],
+    szam(D),
+    szam(E),
+    M is 1,
+    szam(N),
+    szam(O),
+    szam(R),
+    szam(S),
+    szam(Y),
+    D\=E, D\=M, D\=N, D\=O, D\=R, D\=S, D\=Y,
+    E\=M, E\=N, E\=O, E\=R, E\=S, E\=Y,
+    M\=N, M\=O, M\=R, M\=S, M\=Y,
+    N\=O, N\=R, N\=S, N\=Y,
+    O\=R, O\=S, O\=Y,
+    R\=S, R\=Y,
+    S\=Y,
+    S \= 0, M \= 0,
+    A is D + 10*N + 100*E + 1000*S,
+    B is E + 10*R + 100*O + 1000*M,
+    C is Y + 10*E + 100*N + 1000*O + 10000*M,
+    CC is A+B,
+    C = CC.
+
+
+% 10.	Adott a filmek adatbázis (movies.zip) .
+% Az adatbázist be tudjuk olvasni a Prolog-ba (a rendszer indítása után) a
+%               ?- consult(movies),
+% paranccsal (figyeljünk arra, hogy a movies.pl az aktuális könyvtárban legyen)
+%    (rövid formában ez: [movies]. )
+
+%   a.) Keressük meg azon színészeket (actor vagy actress),
+%       akik csak egy filmben játszanak.
+
+lista_actor(L) :-
+    findall(LL, actor(_,LL,_), L).
+
+lista_actress(L) :-
+    findall(LL, actress(_,LL,_), L).
+
+bejar([],[]).
+
+bejar([[_,_]|Com],Rest) :-
+    bejar(Com,Rest).
+
+bejar([[A,1]|Com],[A|Rest]) :-
+    bejar(Com,Rest).
+
+egyszer(Out,Out_L) :-
+    lista_actor(LM),
+    lista_actress(LF),
+    append(LM, LF, LN),
+    sort(LN, LNS),
+    kompakt(LNS,LC),
+    bejar(LC,Out),
+    length(Out,Out_L).
+
+%   b.) Keressük meg azon színészeket (actor vagy actress),
+%       akik pontosan két filmben játszanak.
+
+bejar2([],[]).
+
+bejar2([[_,_]|Com],Rest) :-
+    bejar2(Com,Rest).
+
+bejar2([[A,2]|Com],[A|Rest]) :-
+    bejar2(Com,Rest).
+
+ketszer(Out,Out_L) :-
+    lista_actor(LM),
+    lista_actress(LF),
+    append(LM, LF, LN),
+    sort(LN, LNS),
+    kompakt(LNS,LC),
+    bejar(LC,Out),
+    length(Out,Out_L).
+
+
+%   c.) az adatbázisban találunk színészeket,
+%       akik nem játszottak egy filmben sem?

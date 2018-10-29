b741966c82a41f54ab81ee86df471db72baf474a
Tomi Olah <tomiolah1998@gmail.com>
2018-10-29 14:00:40 +0200

    f

---
+++ 1. FELEV/NUM_MOD/Lab4/CholeskyDecomp.m
@@ -0,0 +1,36 @@
+function L = CholeskyDecomp(A)
+	n = length(A);
+	L = zeros(n);
+	
+	if (A~=transpose(A))
+		disp("Nem szimmetrikus!")
+		return;
+	end
+	
+	for i=1:n
+		for j=1:i
+			if (i==j)
+				sum1 = 0;
+				
+				for k=1:(i-1)
+					sum1 = sum1 + L(i,k)^2;
+				end
+				
+				if (A(i,i) - sum1) < 0
+					disp("Nem pozitiv definit")
+					return;
+				end
+				
+				L(i,i) = sqrt(A(i,i) - sum1);
+			else
+				sum2 = 0;
+				
+				for k=1:(j-1)
+					sum2 = sum2 + L(j,k)*L(i,k);
+				end
+				
+				L(i,j) = (1/L(j,j)) * (A(i,j) - sum2);
+			end
+		end
+	end
+end
---
+++ 1. FELEV/NUM_MOD/Lab4/LUPDecomp.m
@@ -0,0 +1,27 @@
+function [L,U,P] = LUPDecomp(A)
+	n = length(A);
+	L = zeros(n);
+	U = zeros(n);
+	P = eye(n);
+
+	for k=1:(n-1)
+		[m,io] = max(abs(A(k:n,k)));
+		io = io + k - 1;
+		aux = A(io,:);
+		A(io,:) = A(k,:);
+		A(k,:) = aux;
+		P([k io],:) = P([io k],:);
+		
+		if (m==0)
+			return;
+		end	
+		
+		for i=(k+1):n
+			l = A(i,k) / A(k,k);
+			A(i,k:n) = A(i,k:n) - l * A(k,k:n);
+			A(i,k) = l;
+		end
+	end
+	U=triu(A);
+	L=tril(A,-1)+eye(n);
+end

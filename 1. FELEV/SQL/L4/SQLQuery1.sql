adbecdf6056502b1de6afd999e3b7c634fb4c600
Tomi Olah <tomiolah1998@gmail.com>
2018-11-20 12:55:18 +0200

    f

--- 1. FELEV/SQL/L4/SQLQuery1.sql
+++ 1. FELEV/SQL/L4/SQLQuery1.sql
@@ -32,27 +32,43 @@ INSERT INTO @sajat(CikkID,CikkCim,Datum,Szoveg,SzerzoID,KategoriaID,Ertekeles) V
 (13,'Seged3', '2018-10-10', null, 1, 4, 3),
 (14,'Seged4', '2018-10-10', null, 1, 5, 3)
 
-SELECT DISTINCT Felhasznalok.FelhasznaloID, Felhasznalok.FelhasznaloNev, Felhasznalok.EmailCim INTO #a FROM Felhasznalok 
 
 DECLARE @i INT = 1;
 DECLARE @max INT = (SELECT COUNT(*) FROM Kategoriak)
+DECLARE @a TABLE(
+	FelhasznaloID INT,
+	FelhasznaloNev VARCHAR(20),
+	EmailCim VARCHAR(20)
+)
+
+INSERT INTO @a(FelhasznaloID,FelhasznaloNev,EmailCim)
+SELECT DISTINCT Felhasznalok.FelhasznaloID, Felhasznalok.FelhasznaloNev, Felhasznalok.EmailCim
+FROM Felhasznalok
 
 WHILE (@i<=@max)
 BEGIN
-	DROP TABLE #aa
+	DECLARE @x TABLE(
+		FelhasznaloID INT,
+		FelhasznaloNev VARCHAR(20),
+		EmailCim VARCHAR(20)
+	)
 
-	SELECT Felhasznalok.FelhasznaloID, Felhasznalok.FelhasznaloNev, Felhasznalok.EmailCim INTO #aa FROM Felhasznalok, @sajat AS S, #a AS a, Kategoriak
+	DELETE FROM @x
+
+	INSERT INTO @x
+	SELECT Felhasznalok.FelhasznaloID, Felhasznalok.FelhasznaloNev, Felhasznalok.EmailCim FROM Felhasznalok, @sajat AS S, @a AS a, Kategoriak
 	WHERE Felhasznalok.FelhasznaloID=S.SzerzoID AND S.KategoriaID=Kategoriak.KategoriaID
 	AND S.KategoriaID = @i AND a.FelhasznaloID=Felhasznalok.FelhasznaloID
 
-	DROP TABLE #a
-
-	SELECT * INTO #a FROM #aa
+	DELETE FROM @a
+	
+	INSERT INTO @a
+	SELECT DISTINCT FelhasznaloID, FelhasznaloNev, EmailCim FROM @x
 
 	SET @i += 1;
 END
 
-	SELECT FelhasznaloNev, EmailCim FROM #a
+	SELECT FelhasznaloNev, EmailCim FROM @a
 
 --SELECT Felhasznalok.FelhasznaloNev, Felhasznalok.FelhasznaloID FROM Felhasznalok, #a
 --WHERE
@@ -62,10 +78,46 @@ END
 --	4. Adjuk meg kategoriakon belul szerzonkent a cikkek maximalis ertekeleset!
 --	(Kategoriak.KategoriaNev, Felhasznalok.FelhasznaloNev, MaxCikkErtekeles)
 
---SELECT Kategoriak.KategoriaNev, Felhasznalok.FelhasznaloNev FROM Kategoriak, Felhasznalok, Cikkek
---WHERE Kategoriak.KategoriaID=Cikkek.KategoriaID AND Cikkek.SzerzoID=Felhasznalok.FelhasznaloID
+DECLARE @s2 TABLE(
+	KategoriaNev VARCHAR(30),
+	FelhasznaloNev VARCHAR(20),
+	MaxCikkErtekeles INT
+)
 
---	5. Adjuk meg azon felhasznalo(ka)t, aki(k) minimum 3x hozzaszolt(ak) a legkisebb ertekelessel rendelkezo cikk(ek)hez! (Felhasznalok.FelhasznaloNev)
+DECLARE @iii INT = 1;
+SET @iii = 1
+DECLARE @max2 INT = (SELECT COUNT(*) FROM Kategoriak)
+
+WHILE(@iii<=@max2)
+BEGIN
+	
+	DECLARE @KatNev VARCHAR(30) = (SELECT Kategoriak.KategoriaNev FROM Kategoriak WHERE Kategoriak.KategoriaID=@iii)
+	DECLARE @ii INT = 1
+	DECLARE @n INT = (SELECT MAX(FelhasznaloID) FROM Felhasznalok)
+
+	WHILE (@ii<=@n)
+	BEGIN
+
+		DECLARE @fnev VARCHAR(20) = (SELECT Felhasznalok.FelhasznaloNev FROM Felhasznalok WHERE FelhasznaloID=@ii)
+		DECLARE @max_szem INT = (SELECT MAX(Ertekeles) FROM Cikkek WHERE Cikkek.SzerzoID=@ii AND Cikkek.KategoriaID=@iii)
+
+		PRINT @max_szem
+
+		IF @max_szem IS NOT NULL
+		BEGIN
+			PRINT @fnev
+			INSERT INTO @s2(KategoriaNev,FelhasznaloNev,MaxCikkErtekeles) VALUES (@KatNev,@fnev,@max_szem)
+		END
+
+		SET @ii += 1
+	END
+
+	SET @iii += 1
+END
+SELECT * FROM @s2
+--	5. Adjuk meg azon felhasznalo(ka)t, aki(k) minimum 3x hozzaszolt(ak) a
+--	legkisebb ertekelessel rendelkezo cikk(ek)hez!
+--	(Felhasznalok.FelhasznaloNev)
 
 --	6. Adjuk meg minden kulcsszo eseten a hozzajuk rendelt cikk(ek) ertekelesenek atlagat, ezen atlag szerint csokkeno sorrendbe rendezve a kulcsszavakat! (Kulcsszavak.KulcsszoNev, AtlagErtekeles)
 
